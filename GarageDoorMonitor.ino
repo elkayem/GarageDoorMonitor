ba6ce327b078dbba6d049906bbb12a88ff5b2392
Larry McGovern <lkmcgov@gmail.com>
2017-01-08 09:46:52 -0800

    Adding GPL license

--- GarageDoorMonitor.ino
+++ GarageDoorMonitor.ino
@@ -1,3 +1,18 @@
+/*    
+ *    GarageDoorMonitor
+ *	  Copyright (C) 2016  Larry McGovern
+ *	
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License <http://www.gnu.org/licenses/> for more details.
+ */
+ 
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
